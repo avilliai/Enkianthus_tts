2c4049a495b8d3c0e44dfac89a1587185d52a9a0
DEII <1840094972@qq.com>
2024-04-26 00:04:21 +0800

    F

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
-
+with open("README.md", "r",encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name='modelscope_tts',
-    version='0.1.0',
+    version='0.1.2',
     url='https://github.com/avilliai/modelscopeTTS',
     author='avillia',
     author_email='larea06@gmail.com',
