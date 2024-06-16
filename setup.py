26ee7a22b91cfc85cd6e1b246b96c661822c68b0
DEII <1840094972@qq.com>
2024-06-16 13:20:34 +0800

    F

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
     name='Enkianthus_tts',
-    version='0.1.7',
+    version='0.1.8',
     url='https://github.com/avilliai/modelscopeTTS',
     author='avillia',
     author_email='larea06@gmail.com',
@@ -13,5 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'httpx',
+        'websockets',
+        'requests',
      ],
 )
